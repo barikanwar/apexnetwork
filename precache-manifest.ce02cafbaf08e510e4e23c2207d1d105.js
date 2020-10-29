aeb2e7d39704c93f524e056fb4e8bee286ed9fdd
Anwar <400221626@genpact.com>
2020-10-29 15:24:31 +0530

    Updates

---
+++ precache-manifest.ce02cafbaf08e510e4e23c2207d1d105.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d17106c28e7c30498f04e5844c80ab",
+    "url": "/apexnetwork/index.html"
+  },
+  {
+    "revision": "994fd3a590df3695fb7d",
+    "url": "/apexnetwork/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "537174b79ab9e447c6a3",
+    "url": "/apexnetwork/static/css/main.79dc7cb1.chunk.css"
+  },
+  {
+    "revision": "994fd3a590df3695fb7d",
+    "url": "/apexnetwork/static/js/2.80f3acb3.chunk.js"
+  },
+  {
+    "revision": "0f0d2001f7b12eace3acec8fb5ee12a6",
+    "url": "/apexnetwork/static/js/2.80f3acb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537174b79ab9e447c6a3",
+    "url": "/apexnetwork/static/js/main.8eeb2f9a.chunk.js"
+  },
+  {
+    "revision": "7df91d3a3389b97965b6",
+    "url": "/apexnetwork/static/js/runtime-main.5476f727.js"
+  },
+  {
+    "revision": "8c52c7c973898db689278ae76128d296",
+    "url": "/apexnetwork/static/media/1.8c52c7c9.jpg"
+  },
+  {
+    "revision": "2dc96523bdd54f1abfa19df6f8b19059",
+    "url": "/apexnetwork/static/media/2.2dc96523.jpg"
+  },
+  {
+    "revision": "cdb5e50a76ec541cd940011e66ca9882",
+    "url": "/apexnetwork/static/media/3.cdb5e50a.png"
+  },
+  {
+    "revision": "d942d77b8d3d048b7f3eb7e9fae5ce21",
+    "url": "/apexnetwork/static/media/4.d942d77b.jpg"
+  },
+  {
+    "revision": "2532db1e58d4f326b29cbde2db95ac6e",
+    "url": "/apexnetwork/static/media/5.2532db1e.jpg"
+  },
+  {
+    "revision": "48125aa33b7a0b105fc8fbbd7d1b114b",
+    "url": "/apexnetwork/static/media/5.48125aa3.webp"
+  },
+  {
+    "revision": "0f09af0eda6b7e3401f06e398a8736a2",
+    "url": "/apexnetwork/static/media/boximage1PNG.0f09af0e.PNG"
+  },
+  {
+    "revision": "e7432003670c7061f9ada3eff883a63b",
+    "url": "/apexnetwork/static/media/broadband.e7432003.png"
+  },
+  {
+    "revision": "2f86ad1d5a3ccbc1d4953e9cbd4b3738",
+    "url": "/apexnetwork/static/media/network.2f86ad1d.png"
+  },
+  {
+    "revision": "0a5cc0c7d8f4f5a3e5163ada398f1981",
+    "url": "/apexnetwork/static/media/ownPackage.0a5cc0c7.PNG"
+  }
+]);
